c576dd2f604afff1322a1319d594c2a660e95fcb
Sasha <54563756+azora929@users.noreply.github.com>
2026-03-27 15:57:12 +0300

    Фиксы + увелечение фоток

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ function scrollToNext() {
 }
 
 const preloader = document.getElementById('preloader');
+const themeColor = '#ff9a9e';
+
+function applyThemeColor() {
+  document.querySelectorAll('meta[name="theme-color"]').forEach((meta) => {
+    meta.setAttribute('content', themeColor);
+  });
+  const navMeta = document.querySelector('meta[name="msapplication-navbutton-color"]');
+  if (navMeta) navMeta.setAttribute('content', themeColor);
+}
+
+applyThemeColor();
 window.addEventListener('load', () => {
   const preloaderDuration = 3800;
   const fadeDuration = 900;
 
   setTimeout(() => {
+    applyThemeColor();
     preloader.classList.add('preloader--hide');
     document.body.classList.remove('is-loading');
     document.body.classList.add('shots-in');
